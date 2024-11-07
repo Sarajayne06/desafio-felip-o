4f48b063370057e12004ab1b88907786f1e925b5
Sara <sarajayne968@gmail.com>
2024-11-07 20:41:24 -0300

    meu projeto

---
+++ indexx.js
@@ -0,0 +1,18 @@
+let nickname = "Klaus";
+console.log("Bem-Vindo " + nickname);
+
+let fase = "Você passou da fase de resgate da princesa";
+let recompensa = " e recebeu 2.000 mil XP";
+console.log(fase + recompensa);  
+
+let missao = "Você recebeu um bônus de login diário:";
+let bonus = " 1.000 mil XP";
+console.log(missao + bonus);
+
+let pontosColetados = 3000; 
+let item = "XP";
+let premio = "Prata!"
+
+if (pontosColetados === 3000) {
+        console.log("Você ganhou o prêmio: " + premio);
+        }
